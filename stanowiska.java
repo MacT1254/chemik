0858319a40f4f59e94457a853dbd83bbe5ff94e1
MacT1254 <72868402+MacT1254@users.noreply.github.com>
2020-11-21 15:16:25 +0100

    Add files via upload

---
+++ stanowiska.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt;
+
+/**
+ *
+ * @author Mac
+ */
+public class stanowiska {
+
+    public int run_point_x,run_point_y;
+    
+    stanowiska(int run_point_x, int run_point_y){
+        this.run_point_x=run_point_x;
+        this.run_point_y=run_point_y;
+    }
+}
+    
+        
+        
